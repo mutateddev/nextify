3fb8c849073e192cc6ea8edd2fcd7236fceac605
mutateddev <mutateddev@gmail.com>
2026-06-28 11:05:05 +0330

    feat: add sidebar layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Navbar from '@/components/ui/Navbar';
+import Sidebar from '@/components/layout/Sidebar';
+import Navbar from '@/components/layout/Navbar';
 
 export default function Home() {
   return (
     <div className='h-full'>
       <Navbar />
+      <main>
+        <Sidebar />
+      </main>
     </div>
   );
 }
