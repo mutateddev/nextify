8b29dcc86f2d018bc9d8e948dd31ad6981bcd625
mutateddev <mutateddev@gmail.com>
2026-06-28 13:45:06 +0330

    feat: implement music player functionality

--- src/components/features/music/components/MusicPlayer.tsx
+++ src/components/features/music/components/MusicPlayer.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   ListMusic,
   Pause,
@@ -6,12 +8,105 @@ import {
   SkipBack,
   SkipForward,
   Volume2,
+  VolumeOff,
 } from 'lucide-react';
 import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
 
 const MusicPlayer = () => {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(77);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [prevVol, setPrevVol] = useState(0);
+
+  const togglePlayButton = () => {
+    if (!audioRef.current) return;
+
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    }
+
+    if (!isPlaying) {
+      audioRef.current.play();
+      setIsPlaying(true);
+    }
+  };
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60)
+      .toString()
+      .padStart(2, '0');
+
+    return `${minutes}:${seconds}`;
+  };
+
+  const handleSeek = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newTime = parseFloat(e.target.value);
+    if (audioRef.current) {
+      audioRef.current.currentTime = newTime;
+      setCurrentTime(newTime);
+    }
+  };
+
+  const handleVolume = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const volume = parseInt(e.target.value);
+    setVolume(volume);
+    if (audioRef.current) {
+      audioRef.current.volume = volume / 100;
+    }
+  };
+
+  const toggleMute = () => {
+    if (volume === 0) {
+      setVolume(prevVol);
+      if (audioRef.current) {
+        audioRef.current.volume = prevVol / 100;
+      }
+    }
+    if (volume !== 0) {
+      setPrevVol(volume);
+      setVolume(0);
+      if (audioRef.current) {
+        audioRef.current.volume = 0;
+      }
+    }
+  };
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    if (!audio) return;
+
+    const updateTime = () => {
+      setCurrentTime(audio.currentTime);
+      setDuration(audio.duration || 0);
+    };
+
+    audio.addEventListener('timeupdate', updateTime);
+    audio.addEventListener('loadedmetadata', updateTime);
+
+    return () => {
+      audio.removeEventListener('timeupdate', updateTime);
+      audio.removeEventListener('loadedmetadata', updateTime);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = volume / 100;
+    }
+  }, [volume]);
+
   return (
     <div className='fixed bottom-0 left-0 w-full bg-bg/90 backdrop-blur-md border-t border-border text-text px-4 py-3 shadow-lg z-50'>
+      <audio
+        src='/music/brain-boost.mp3'
+        ref={audioRef}
+        className='hidden'
+      ></audio>
       <div className='max-w-7xl mx-auto flex items-center justify-between gap-6'>
         {/* LEFT - SONG INFO */}
         <div className='flex items-center gap-4 min-w-50'>
@@ -36,8 +131,11 @@ const MusicPlayer = () => {
               <SkipBack size={20} />
             </button>
 
-            <button className='w-10 h-10 flex items-center justify-center rounded-full bg-primary text-black shadow-md hover:scale-105 transition'>
-              <Pause size={18} />
+            <button
+              onClick={togglePlayButton}
+              className='w-10 h-10 flex items-center justify-center rounded-full bg-primary text-black shadow-md hover:scale-105 transition cursor-pointer'
+            >
+              {isPlaying ? <Pause size={18} /> : <Play size={18} />}
             </button>
 
             <button className='text-text-muted hover:text-text transition'>
@@ -47,16 +145,18 @@ const MusicPlayer = () => {
 
           {/* PROGRESS */}
           <div className='w-full flex items-center gap-2 text-xs'>
-            <span className='text-text-muted'>1:45</span>
+            <span className='text-text-muted'>{formatTime(currentTime)}</span>
 
             <input
               type='range'
+              onChange={handleSeek}
               min={0}
-              max={100}
+              max={duration || 0}
+              value={currentTime}
               className='w-full h-1 accent-primary cursor-pointer'
             />
 
-            <span className='text-text-muted'>5:05</span>
+            <span className='text-text-muted'>{formatTime(duration)}</span>
           </div>
         </div>
 
@@ -71,10 +171,17 @@ const MusicPlayer = () => {
           </button>
 
           <div className='flex items-center gap-2'>
-            <Volume2 size={18} className='text-text-muted' />
+            <div
+              onClick={toggleMute}
+              className='text-text-muted cursor-pointer'
+            >
+              {volume === 0 ? <VolumeOff size={18} /> : <Volume2 size={18} />}
+            </div>
 
             <input
               type='range'
+              onChange={handleVolume}
+              value={volume}
               min={0}
               max={100}
               className='w-22.5 h-1 accent-primary cursor-pointer'
